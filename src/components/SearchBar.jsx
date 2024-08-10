1cf24f69cf9903b54960e3e3c117b9bcda63fecc
Valerio Casadei <valeriocasadei18@gmail.com>
2024-08-10 13:01:16 +0200

    More updates

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,22 +1,29 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-import { Col, Form, Row } from "react-bootstrap";
+import { Col, Form, InputGroup, Row } from "react-bootstrap";
 
 const SearchBar = ({ handleChange, handleSubmit, input }) => {
 	return (
 		<div>
 			<h1>To Do List</h1>
 			<Form onSubmit={handleSubmit}>
-				<Row className="justify-content-center">
+				<Row className="justify-content-center mt-4 mb-5">
 					<Col xs={12} sm={8} md={5} xl={3}>
-						<Form.Control value={input} type="search" id="input" placeholder="scrivi" onChange={handleChange}>
-							<FontAwesomeIcon icon={faMagnifyingGlass} />
-						</Form.Control>
+						<InputGroup>
+							<Form.Control
+								className="formControl"
+								value={input}
+								type="search"
+								id="input"
+								placeholder="scrivi"
+								onChange={handleChange}
+							></Form.Control>
+							<button className="rounded-5 px-3 ms-2" type="submit" htmlFor="input">
+								<FontAwesomeIcon icon={faMagnifyingGlass} />
+							</button>
+						</InputGroup>
 					</Col>
 				</Row>
-				<button type="submit" htmlFor="input">
-					Search
-				</button>
 			</Form>
 		</div>
 	);
