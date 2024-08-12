1921ae253850f198bfacbf9edd15ac4f1176a4f8
Valerio Casadei <valeriocasadei18@gmail.com>
2024-08-12 13:25:55 +0200

    CSS update

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -6,10 +6,14 @@ const List = ({ todo, handleDelete }) => {
 			<Row className="gy-3 justify-content-center">
 				{todo &&
 					todo.map((e, i) => (
-						<Col className="p-2 postit" key={i} xs={12} md={5} lg={4}>
-							<h3 className="my-2">To Do:</h3>
-							<p>{e}</p>
-							<button onClick={() => handleDelete(i)}>Cancella</button>
+						<Col className="postit p-2 d-flex flex-column justify-content-between" key={i} xs={12} md={5} lg={4}>
+							<h3 className="my-2">To Do {i + 1}:</h3>
+							<div className="d-flex justify-content-center align-items-center">
+								<p className="font px-1">{e.length > 50 ? e.slice(0, 49) + "..." : e}</p>
+							</div>
+							<button className=" w-50" onClick={() => handleDelete(i)}>
+								Cancella
+							</button>
 						</Col>
 					))}
 			</Row>
